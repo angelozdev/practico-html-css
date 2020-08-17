0acd72dea052f5a0b2b642e4ba8531bc9d610dca
Angelo Zambrano <angelozam17@gmail.com>
2020-08-17 10:21:40 -0500

    Initial commit

---
+++ src/index.stories.js
@@ -0,0 +1,6 @@
+import '../CSS/main.css'
+
+export default { title: 'Components' };
+
+export const searchGoogle = () => `<section class="search-form"><div class="search-form-container"><label for="search" class="search--icon"></label><input type="text" id="search"/><a href="#" class="micro-icon"></a></div></section>`;
+export const footer = () => '<footer class="footer"><ul class="footer-left"><li><a href="#">Publicidad</a></li><li><a href="#">Negocios</a></li><li><a href="#">Todo sobre Google</a></li><li><a href="#">Cómo funciona la búsqueda</a></li></ul><ul class="footer-right"><li><a href="#">Privaciadad</a></li><li><a href="#">Condiciones</a></li><li><a href="#">Preferencias</a></li></ul></footer>';
